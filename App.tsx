1be2f79c5c86d454ad14a57a6b76ecbf1429ed19
mmertsahinn <146315020+mmertsahinn@users.noreply.github.com>
2025-11-16 13:13:18 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { SplashScreen } from './components/SplashScreen';
+
+export default function App() {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <SplashScreen />
+    </div>
+  );
+}
